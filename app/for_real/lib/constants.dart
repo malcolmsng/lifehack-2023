5602abb993c66a56f5f6e58945e29c2cfbd3db41
Malcolm Sng <malcolmsng99@gmail.com>
2023-05-28 17:16:57 +0800

    initial commit

---
+++ app/for_real/lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// colors
+const Color hotPink = Color(0xFFf72585);
+const Color purple = Color(0xff7209b7);
+const Color violet = Color(0xff3a0ca3);
+const Color blue = Color(0xff4361ee);
+const Color cyan = Color(0xff4361ee);
+const Color offWhite = Color(0xfff5f5f5);
+// fontSizes
