a3f04d1b5f9e256570b9cf14ecc2037cedc640f1
Yam Kar Lok <isonice4572@gmail.com>
2023-05-30 01:17:02 +0800

    added log messages constants

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -8,6 +8,31 @@ const CORS: Readonly<CorsOptions> = {
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
 };
 
+const LOG_MESSAGES = {
+  TRACE: {
+    ZOD_PARSING: (schemaName: string) =>
+      `Parsing JSON into Zod Schema (${schemaName}).` as const,
+    TRANSACTION: {
+      SELLER_INITIATED: (sellerId: string, txHash: string) =>
+        `Seller (${sellerId}) successfully initiated transaction (${txHash}).` as const,
+      BUYER_ACCEPTED: (buyerId: string, txHash: string) =>
+        `Buyer (${buyerId}) accepted transaction (${txHash}).` as const,
+      PENDING_VALIDATOR: (
+        transactionHash: string,
+        validatorType: 'internal' | 'manual' | 'auto'
+      ) =>
+        `Transaction (${transactionHash}) is pending ${validatorType} validator.`,
+    },
+  },
+  ERROR: {
+    INVALID_BODY: 'Invalid JSON Body.',
+    TX_HASH_NOT_FOUND: (hash: string) =>
+      `Cannot find the specific transaction hash: ${hash}.` as const,
+    UNKNOWN: 'Unknown error. Returned 500 to client.',
+  },
+} as const;
+
 export default {
   CORS,
+  LOG_MESSAGES,
 } as const;
