0fbf3443d48ac501628af6276771fea4f5b6c9dd
Yam Kar Lok <isonice4572@gmail.com>
2023-05-30 09:14:20 +0800

    testing stuff for on-chain

--- on-chain-program/tests/on-chain-program.ts
+++ on-chain-program/tests/on-chain-program.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, SystemProgram } from "@coral-xyz/anchor";
 import { OnChainProgram } from "../target/types/on_chain_program";
+import { assert } from "chai";
 
 describe("on-chain-program", () => {
   // Configure the client to use the local cluster.
@@ -10,7 +11,20 @@ describe("on-chain-program", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const myAccount = anchor.web3.Keypair.generate();
+
+    const tx = await program.rpc.initialize({
+      accounts: {
+        myAccount: myAccount.publicKey,
+        user: anchor.getProvider().publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers: [myAccount],
+    });
     console.log("Your transaction signature", tx);
+
+    const account = await program.account.myAccount.fetch(myAccount.publicKey)
+
+    assert.ok(account.data.eq(new anchor.BN(0)))
   });
 });
